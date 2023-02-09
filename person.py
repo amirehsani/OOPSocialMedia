0c394cd02148134ad0c7bf23a4983ba9eca28b0a
Amir Ehsani <65291639+amirehsani@users.noreply.github.com>
2023-02-09 13:08:05 +0330

    Added age/phone number verification ability.

--- person.py
+++ person.py
@@ -1,36 +1,68 @@
 class User:
 
     def __init__(self, username, phone_number, age):
-        self.username = None
-        self.phone_number = None
+
+        self.username = username
+        self.phone_number = phone_number
         self.age = age
 
-    @property
-    @ageVerification.getter
-    def ageVerification(self):
-
-        return self.__age = age
-
-    @ageVerification.setter
-    def ageVerification(self, a):
-
-        if self.age < 18:
-            raise ValueError("Sorry, your age is below our eligibility policies.")
-
-        self.age = age
-
+    # USERNAME VALIDATION
+    # GETTER
     @property
     def username(self):
-        return self.__username.lower()
 
+        return self._username
+
+    # SETTER
     @username.setter
-    def username(self, value):
-        self.__username = username
+    def username(self, username_value):
+
+        self._username = username_value.lower()
+
+    # AGE VERIFICATION
+    # GETTER
+    @property
+    def age(self):
+
+        return self._age
+
+    # SETTER
+    @age.setter
+    def age(self, age_value):
+
+        if type(age_value) is not int:
+            raise TypeError('Age must be a valid number!')
+
+        if age_value > 0:
+            raise ValueError('Your age cannot be a negative number!')
+
+        elif 13 >= age >= 0:
+            raise ValueError(
+                "Sorry, your age is below our eligibility policies")
+
+        else:
+            self._age = age_value
+
+    # PHONE NUMBER VALIDATION BASED ON +989123456789
+    # GETTER
 
     @property
     def phone_number(self):
-        return self.__phone_number
 
+        return self._phone_number
+
+    # SETTER
     @phone_number.setter
-    def phone_number(self, number):
-        self.__phone_number = number
+    def phone_number(self, phone_number_value):
+
+        phone_number = [x for x in str(phone_number_value)]
+
+        if len(phone_number_value) != 13:
+
+            if phone_number[0] == '+' and phone_number[1] == '9' and phone_number[2] == '8':
+
+                self._phone_number = phone_number_value
+
+            raise ValueError("Please enter a valid number!")
+
+        raise ValueError("Please enter a valid number!")
